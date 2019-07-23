93f355893adc5f2f8c80d54046c67cbf958c33e0
TimothyErcia <timothy.ercia@gmail.com>
2019-07-23 13:43:58 +0800

    commit 7/23/19

--- SolutionDay11.py
+++ SolutionDay11.py
@@ -4,6 +4,13 @@ You are given an N by M matrix of 0s and 1s. Starting from the top left corner,
 how many ways are there to reach the bottom right corner?
 You can only move right and down.
 0 represents an empty space while 1 represents a wall you cannot walk through.
+
+example
+[[0,0,1],
+[0,0,1],
+[1,0,0]]
+Starting from [0][0] to [2][2]
+moves = [Right, down, down, right]
 """
 
 def reachBottom(items):
@@ -13,28 +20,31 @@ def reachBottom(items):
 
     max_position = len(items) - 1
 
-    while position_X is not max_position and position_Y is not max_position:
-        if  items[position_Y][position_X + 1] == 1:
-            moves.append("Down")
-            position_Y += 1
+    try:
+        while position_X is not max_position or position_Y is not max_position:
+            if items[position_Y][position_X + 1] == 1:
+                moves.append("Down")
+                position_Y += 1
         
-        elif items[position_Y][position_X + 1] == 0:
-            moves.append("Right")
-            position_X += 1
-        
-    if items[position_Y][position_X] == 1:
-            moves.append("Down")
-            position_Y += 1
-        
-    elif items[position_Y][position_X] == 0:
-            moves.append("Right")
-            position_X += 1
+            elif items[position_Y][position_X + 1] == 0:
+                moves.append("Right")
+                position_X += 1
+            
+    except:
+        while position_X is not max_position or position_Y is not max_position:
+            if position_X == max_position:
+                moves.append("Down")
+                position_Y += 1
+
+            elif position_Y == max_position:
+                moves.append("Right")
+                position_X += 1
+
     return moves
 
-
-item2 = [[0,0,1],
-        [1,0,1],
-        [1,0,0]]
+item2 = [[0,0,0],
+        [0,1,0],
+        [0,0,0]]
 
 rb = reachBottom(item2)
 print(rb)
